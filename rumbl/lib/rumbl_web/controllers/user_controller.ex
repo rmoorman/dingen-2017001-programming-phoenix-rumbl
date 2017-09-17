49d47de7e37e0a4ef5a8c4c01c1e433f0a9037ab
Rico Moorman <rico.moorman@gmail.com>
2017-09-18 00:19:01 +0200

    Add new user form

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Repo
+  alias Rumbl.User
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
@@ -10,5 +11,10 @@ defmodule RumblWeb.UserController do
   def show(conn, %{"id" => id}) do
      user = Repo.get(Rumbl.User, id)
      render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
