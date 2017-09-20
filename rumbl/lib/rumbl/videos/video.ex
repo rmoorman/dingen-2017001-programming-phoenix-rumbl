cc98f23b74206ac07664381d1e67970f193be02d
Rico Moorman <rico.moorman@gmail.com>
2017-09-20 22:27:28 +0200

    Validating Unique Data

--- rumbl/lib/rumbl/videos/video.ex
+++ rumbl/lib/rumbl/videos/video.ex
@@ -16,13 +16,14 @@ defmodule Rumbl.Videos.Video do
   end
 
 
-  @permitted_fields = [:url, :title, :description, :category_id]
-  @required_fields = [:url, :title, :description]
+  @permitted_fields [:url, :title, :description, :category_id]
+  @required_fields [:url, :title, :description]
 
   @doc false
   def changeset(%Video{} = video, attrs) do
     video
     |> cast(attrs, @permitted_fields)
+    |> assoc_constraint(:category)
     |> validate_required(@required_fields)
   end
 end
