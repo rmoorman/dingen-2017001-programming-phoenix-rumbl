58b11da48d683a4a8fc3db5deefc1419982bf302
Rico Moorman <rico.moorman@gmail.com>
2017-10-02 02:28:20 +0200

    Building GenServers for OTP

--- rumbl/lib/rumbl/counter.ex
+++ rumbl/lib/rumbl/counter.ex
@@ -1,29 +1,20 @@
 defmodule Rumbl.Counter do
-  # otp initialization
-  def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
-  end
+  use GenServer
 
   # client part
-  def inc(pid), do: send(pid, :inc)
-  def dec(pid), do: send(pid, :dec)
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-    receive do
-      {^ref, val} -> val
-    after timeout -> exit(:timeout)
-    end
+  def inc(pid), do: GenServer.cast(pid, :inc)
+  def dec(pid), do: GenServer.cast(pid, :dec)
+  def val(pid), do: GenServer.call(pid, :val)
+
+  # genserver part
+  def start_link(initial_val) do
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  # server part
-  defp listen(val) do
-    receive do
-      :inc -> listen(val + 1)
-      :dec -> listen(val - 1)
-      {:val, sender, ref} ->
-        send sender, {ref, val}
-        listen(val)
-    end
-  end
+  def init(initial_val), do: {:ok, initial_val}
+
+  def handle_cast(:inc, val), do: {:noreply, val + 1}
+  def handle_cast(:dec, val), do: {:noreply, val - 1}
+
+  def handle_call(:val, _from, val), do: {:reply, val, val}
 end
