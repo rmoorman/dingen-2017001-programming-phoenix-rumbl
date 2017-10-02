49648504437bae42b9e6dfb2697c62bc3e126a57
Rico Moorman <rico.moorman@gmail.com>
2017-10-02 02:41:26 +0200

    Adding Failover

--- rumbl/lib/rumbl/counter.ex
+++ rumbl/lib/rumbl/counter.ex
@@ -11,7 +11,17 @@ defmodule Rumbl.Counter do
     GenServer.start_link(__MODULE__, initial_val)
   end
 
-  def init(initial_val), do: {:ok, initial_val}
+  def init(initial_val) do
+    Process.send_after(self(), :tick, 1000)
+    {:ok, initial_val}
+  end
+
+  def handle_info(:tick, val) when val <= 0, do: raise "boom!"
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self(), :tick, 1000)
+    {:noreply, val - 1}
+  end
 
   def handle_cast(:inc, val), do: {:noreply, val + 1}
   def handle_cast(:dec, val), do: {:noreply, val - 1}
